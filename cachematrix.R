ddedfcb17ebc36526a078eb8d7d7246aed60eafe
unknown <shimbea@BOS301070.pii.pxl.int>
2016-02-21 14:14:50 -0500

    Implemented Programming Assignment 2 for R Programming on Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Defines special wrapper object around matrix that caches inverse form of the matrix.
+## Implements efficient matrix inversion that uses cached matrix economizing on computations
 
-## Write a short comment describing this function
-
+## Provides mechanism for accessing and storing/caching matrix and its inverse form
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(inv) i <<- inv
+  
+  getInverse <- function() i
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Computes the inverse of a square matrix or returns cached inverse matrix if it's available
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setInverse(i)
+  i
+}
+
+
+###############################################################
+## UNIT TEST
+testCacheSolve <- function(){
+  
+  hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+  h8 <- hilbert(8)
+  
+  cacheSolve(makeCacheMatrix(h8))
 }
